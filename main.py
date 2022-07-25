5a3aa7892c288d562c7ce94aac926eb656de92a4
PradeepNaik1 <cheyanoonsavath@gmail.com>
2022-07-25 19:12:40 +0530

    pradeep

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from winreg import HKEY_LOCAL_MACHINE
+
+
 list_elements = [2, 3, 5, 8, 4, 9]
 
 result_list = [index for index in list_elements if (index % 2) == 0]
 
 print(result_list)
+
+r_list = [index for index in list_elements if (index % 3 )== 0]
+
+print(r_list)
